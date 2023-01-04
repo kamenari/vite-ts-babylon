ccba512f033c6202f289e2c80723c043ab25247e
kamenari <kamenaris@gmail.com>
2023-01-04 14:13:00 +0900

    hitTestを追加

--- src/main.ts
+++ src/main.ts
@@ -13,16 +13,46 @@ const main = async () => {
     scene.createDefaultCameraOrLight(true, true, true);
     // scene.createDefaultEnvironment();
 
-    const boxSize = 0.2;
-    const box = BABYLON.MeshBuilder.CreateBox("box", { size: boxSize });
-    box.position.addInPlaceFromFloats(0, boxSize / 2.0, 0);
-    // Cubeの高さを1.6mに設定
-    // box.position.addInPlaceFromFloats(0, 1.6, 0);
-
-    await scene.createDefaultXRExperienceAsync({
+    const xr =  await scene.createDefaultXRExperienceAsync({
       uiOptions: {
         sessionMode: 'immersive-ar',
       },
+      // webXR Featuresを有効にする
+      optionalFeatures: true,
+    });
+
+    // FeaturesManagerを取得
+    const featureManager = xr.baseExperience.featuresManager;
+
+    // hitTestを設定
+    const hitTestOptions: BABYLON.IWebXRHitTestOptions = {
+      enableTransientHitTest: true,
+      disablePermanentHitTest: true,
+      transientHitTestProfile: 'generic-touchscreen',
+    };
+
+    // hitTestを有効にする
+    const hitTest = featureManager.enableFeature(
+      BABYLON.WebXRHitTest,
+      'latest',
+      hitTestOptions,
+    ) as BABYLON.WebXRHitTest;
+
+    hitTest?.onHitTestResultObservable.add((result) => {
+      // コールバックで受け取ったresultが空の場合は処理を終了
+      if (!result.length) {
+        return;
+      }
+
+      const boxSize = 0.2;
+      const box = BABYLON.MeshBuilder.CreateBox("box", { size: boxSize });
+      // box.position.addInPlaceFromFloats(0, boxSize / 2.0, 0);
+      // Cubeの高さを1.6mに設定
+      // box.position.addInPlaceFromFloats(0, 1.6, 0);
+
+      // hitTestの結果をboxの位置に設定
+      box.position = result[0].position;
+      box.rotationQuaternion = result[0].rotationQuaternion;
     });
 
     engine.runRenderLoop(() => {
